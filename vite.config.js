1d4c6c3d46b21f02b5d39368f90714780062e47f
lizzy596 <lizmeek123@gmail.com>
2024-01-09 14:39:21 -0600

    ad

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://lizzy596.github.io/Callanetics-List/'
+  base: '/Callanetics-List/'
 })
